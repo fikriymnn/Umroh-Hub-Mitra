8f79be29109157bd910e93d2f1f29cb6b05a1cc3
IrfanSahruli <irfansahruli244@gmail.com>
2025-06-26 17:05:32 +0700

    Melanjutkan dashboard

---
+++ src/types/Dashboard.ts
@@ -0,0 +1,10 @@
+export interface Dashboard {
+    allJamaah: number;
+    allPackage: number;
+    order: number;
+    packageActive: number;
+    packagePlus: number;
+    packageRegular: number;
+    thisMonth: number;
+    totalRevenueThisMonth: number
+};
