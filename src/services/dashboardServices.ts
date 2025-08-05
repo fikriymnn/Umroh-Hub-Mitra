e8a2bf7cbe21b821101de97625efd2700365baf4
IrfanSahruli <irfansahruli244@gmail.com>
2025-08-05 14:16:36 +0700

    Mengganti endpoint untuk api dashboard mitra

--- src/services/dashboardServices.ts
+++ src/services/dashboardServices.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export async function getDataDashboardMitra () {
-    return await axios.get(`${import.meta.env.VITE_PUBLIC_URL}/getDataSahboardMitra`, {
+    return await axios.get(`${import.meta.env.VITE_PUBLIC_URL}/getDataDahboardMitra`, {
         withCredentials: true
     });
 };
